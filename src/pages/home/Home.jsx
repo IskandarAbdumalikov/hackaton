00a1332e87bad1eeba4a8c472795aa3a7d3ade90
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-30 14:32:10 +0500

    added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,28 +8,30 @@ import cars from "../../assets/cars.jpg";
 
 const Home = () => {
   return (
-    <div className="container home">
-      <div className="home__hero">
-        <h1>NEED FOR SPEED</h1>
-        <h3>UZBEKISTAN</h3>
-      </div>
-      <div className="home__cards">
-        <Link to={"/area"} className="card">
-          <img src={area} alt="" />
-          <h3>Area</h3>
-        </Link>
-        <Link to={"/race"} className="card">
-          <img src={race} alt="" />
-          <h3>Race</h3>
-        </Link>
-        <Link to={"/cars"} className="card">
-          <img src={cars} alt="" />
-          <h3>Cars</h3>
-        </Link>
-        <Link to={"/users"} className="card">
-          <img src={users} alt="" />
-          <h3>Users</h3>
-        </Link>
+    <div className="home">
+      <div className="container">
+        <div className="home__hero">
+          <h1>NEED FOR SPEED</h1>
+          <h3>UZBEKISTAN</h3>
+        </div>
+        <div className="home__cards">
+          <Link to={"/area"} className="card">
+            <img src={area} alt="" />
+            <h3>Area</h3>
+          </Link>
+          <Link to={"/race"} className="card">
+            <img src={race} alt="" />
+            <h3>Race</h3>
+          </Link>
+          <Link to={"/cars"} className="card">
+            <img src={cars} alt="" />
+            <h3>Cars</h3>
+          </Link>
+          <Link to={"/users"} className="card">
+            <img src={users} alt="" />
+            <h3>Users</h3>
+          </Link>
+        </div>
       </div>
     </div>
   );
